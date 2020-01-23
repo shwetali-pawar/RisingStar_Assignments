3ac80a903dec5ed755f0a007c1c17888c18b29f6
shwetali-pawar <57858678+shwetali-pawar@users.noreply.github.com>
2020-01-23 15:00:26 +0530

    Add files via upload

---
+++ ArrayList1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ArrayList1 
+{
+  public static void main(String[] args) 
+  {
+  List<String> list_Strings = new ArrayList<String>();
+  list_Strings.add("Violet");
+  list_Strings.add("Indigo");
+  list_Strings.add("Blue");
+  list_Strings.add("Green");
+  list_Strings.add("Yellow");
+  System.out.println(list_Strings);
+ }
+}
